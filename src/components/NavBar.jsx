d9b88225af75e3c989833a113b8757db6dcfd832
Rianna Cleary <rianna.cleary@gmail.com>
2020-03-25 12:32:22 -0400

    took out login for logged-in users

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,9 +11,6 @@ class NavBar extends Component {
             <NavLink to='/'>Home</NavLink>
           </li>
           <li>
-            <NavLink to='/login'>Login</NavLink>
-          </li>
-          <li>
             <NavLink to='/profile'>Profile</NavLink>
           </li>
           <li>
