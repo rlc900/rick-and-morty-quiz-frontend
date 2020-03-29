5a67449788e163455abd5e364e70ec92701cf41a
Rianna Cleary <rianna.cleary@gmail.com>
2020-03-29 15:15:56 -0400

    finished summers desc

--- src/QuizContainer/Result.jsx
+++ src/QuizContainer/Result.jsx
@@ -12,7 +12,7 @@ class Result extends Component {
       } else if (points === 15 || points <= 18){
         return this.imgTag("rick.png", "Rick", 'Wuuubba lubba dub dub! Rick is the most billiant man in the whole universe, from creating devices that produce portals to different dimensions to interdimensional cable TV boxes. His brillance gets tampered with frquently due to his alcohol addiction and harsh personal views. He has a tendency to be possessive and dominating towards Morty when they go on adventures, but as much as he hates to admit it, Ricks love for morty and their memories will never die.')
       } else if (points === 19 || points <= 21){
-        return this.imgTag("summer.png", "Summer", 'You\'re so totally Summer. Summer is an average teenage girl that loves social media & cares about')
+        return this.imgTag("summer.png", "Summer", 'You\'re so totally Summer. Summer is an average teenage girl that loves social media & popularity. She views her family as a dysfunctional one, and will sometimes get jealous of Rick and Morty\'s relationship. She could have sociopathic/narcissistic attitudes when she is pushed too far, similar to her brother. She\'s not as cold as she seems, and in fact cares about her family more than anyone. She once stated that she would rather live in an isolated existence without a future rather than abadoning Rick, and reminded her father Jerry \'You don\'t love someone for a reward.\'')
       } else if (points === 22 || points <= 24){
         return this.imgTag("beth.png", "Beth", 'You\'re Beth!')
       }
